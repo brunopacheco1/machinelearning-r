1e23c45bf842916964e24979ce1ac2078c04786e
Bruno Pacheco <brunopacheco1@yahoo.com>
2018-03-01 19:55:28 +0100

    Adding Decision Tree and Random Forest classifiers.

---
+++ classification/random_forest.R
@@ -0,0 +1,61 @@
+library(caTools)
+library(ElemStatLearn)
+library(randomForest)
+
+#Importing
+dataset = read.csv('data/social_network_ads.csv')
+dataset = dataset[3:5]
+
+#Encoding the target feature as factor
+dataset$Purchased = factor(dataset$Purchased, levels = c(0, 1))
+
+#Splitting
+set.seed(123)
+split = sample.split(dataset$Purchased, SplitRatio = 0.75)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+#Feature Scaling
+training_set[-3] = scale(training_set[-3])
+test_set[-3] = scale(test_set[-3])
+
+#Fiting
+classifier = randomForest(ntree = 20000, formula = Purchased ~ .,
+                 data = training_set)
+
+#Predicting
+y_pred = predict(classifier, newdata = test_set[-3], type = 'class')
+
+#Confusion Matrix
+cm = table(test_set[, 3], y_pred)
+
+#Visualising Training Set
+set = training_set
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('Age', 'EstimatedSalary')
+y_grid = predict(classifier, newdata = grid_set, type = 'class')
+
+plot(set[, -3],
+     main = 'Random Forest (Training set)',
+     xlab = 'Age', ylab = 'Estimated Salary',
+     xlim = range(X1), ylim = range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
+
+#Visualising Test Set
+set = test_set
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('Age', 'EstimatedSalary')
+y_grid = predict(classifier, newdata = grid_set, type = 'class')
+
+plot(set[, -3], main = 'Random Forest (Test set)',
+     xlab = 'Age', ylab = 'Estimated Salary',
+     xlim = range(X1), ylim = range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
