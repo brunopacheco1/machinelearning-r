79516947dbd4a1ffdb0531f210f3262e15d0b429
Bruno Silva <bruno_dasilva@epam.com>
2018-05-09 15:38:27 +0200

    PCA example.

--- neural_network/ann.R
+++ neural_network/ann.R
@@ -1,7 +1,10 @@
-# Artificial Neural Network
+# install.packages('caTools')
+# install.packages('h2o')
+library(h2o)
+library(caTools)
 
 # Importing the dataset
-dataset = read.csv('Churn_Modelling.csv')
+dataset = read.csv('data/churn_modelling.csv')
 dataset = dataset[4:14]
 
 # Encoding the categorical variables as factors
@@ -13,10 +16,9 @@ dataset$Gender = as.numeric(factor(dataset$Gender,
                                    labels = c(1, 2)))
 
 # Splitting the dataset into the Training set and Test set
-# install.packages('caTools')
-library(caTools)
+
 set.seed(123)
-split = sample.split(dataset$Exited, SplitRatio = 0.8)
+split = sample.split(dataset$Exited, SplitRatio = 0.75)
 training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
 
@@ -24,20 +26,19 @@ test_set = subset(dataset, split == FALSE)
 training_set[-11] = scale(training_set[-11])
 test_set[-11] = scale(test_set[-11])
 
-# Fitting ANN to the Training set
-# install.packages('h2o')
-library(h2o)
+# Fitting
+
 h2o.init(nthreads = -1)
 model = h2o.deeplearning(y = 'Exited',
                          training_frame = as.h2o(training_set),
                          activation = 'Rectifier',
-                         hidden = c(5,5),
-                         epochs = 100,
+                         hidden = c(16,16),
+                         epochs = 1000,
                          train_samples_per_iteration = -2)
 
-# Predicting the Test set results
+# Predicting
 y_pred = h2o.predict(model, newdata = as.h2o(test_set[-11]))
-y_pred = (y_pred > 0.5)
+y_pred = (y_pred > 0.6)
 y_pred = as.vector(y_pred)
 
 # Making the Confusion Matrix
